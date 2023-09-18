c94e900c3576144c0cd38255e83697e33760a860
Awrmani <armanvalaee@gmail.com>
2023-09-18 19:13:24 -0400

    Dynamic search bar added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
 import CardList from "./CardList";
 import { robots } from "./robots";
 import SearchBox from './SearchBox';
 
-const App = () => {
-  return (
-    <div className="tc">
-      <h1>RoboFriends</h1>
-      <SearchBox />
-      <CardList robots={robots} />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      robots: robots,
+      searchfield: ""
+    }
+  }
+
+  onsearchChange = (event) => {
+    this.setState({searchfield: event.target.value});
+    // console.log("Tedst" + event.target.value);
+  }
+
+  render() {
+    const filteredRobots = this.state.robots.filter(robot => {
+      return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    })
+    // console.log(filteredRobots);
+    return (
+      <div className="tc">
+        <h1>RoboFriends</h1>
+        <SearchBox searchChange={this.onsearchChange}/>
+        <CardList robots={filteredRobots} />
+      </div>
+    );
+  }
 };
 
 export default App;
